c41251109e76e1f532b01e92d315943415c86262
Breena <breenatheseira@gmail.com>
2024-03-08 23:16:56 +0800

    Player can jump on platform

---
+++ Player.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+@onready var ray = $RayCast2D
+
+var grounded = false
+var gravity = 15
+var max_y_velocity = 300
+var y_velocity = 0
+var jumping = false
+var jump_speed = -500
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	
+	if not grounded:
+		y_velocity = min(max_y_velocity, y_velocity + gravity)
+	else:
+		y_velocity = 0
+		
+	grounded = false
+	
+	var jump = Input.is_action_just_pressed("jump") and not jumping
+	
+	if jump:
+		jumping = true
+		y_velocity = jump_speed
+	
+	position.y += y_velocity * delta
+	
+	if not jump: 
+		if ray.is_colliding():
+			var origin = ray.global_transform.origin
+			var collision_point = ray.get_collision_point()
+			var distance = abs(origin.y - collision_point.y)
+			var depth = abs(ray.target_position.y - distance)
+			
+			grounded = true
+			jumping = false
+			
+			position.y -= depth
